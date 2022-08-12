45f4526fb3b094ee6ac8c3c4227d1d737442c841
kohrd <konrad.janeczek@protonmail.com>
2022-08-12 09:30:47 +0200

    2

---
+++ lib/main.dart
@@ -0,0 +1,117 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+// container 41
+
+// https://medium.com/flutter-community/flutter-layout-cheat-sheet-5363348d037e
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.cyan[900],
+        body: SafeArea(
+          child: Column(
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50.0,
+                // backgroundColor: Colors.red,
+                backgroundImage: AssetImage('images/gigaChad.jpg'),
+              ),
+              Text(
+                'Giga Chad',
+                style: TextStyle(
+                  fontFamily: 'KaushanScript',
+                  color: Colors.white,
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                'Redaktor naczelny',
+                style: TextStyle(
+                  fontFamily: 'SourceSansPro',
+                  color: Colors.white,
+                  fontSize: 10,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              // Container( // zamieniamy Container na Card
+          Card(
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 30),
+                // padding: EdgeInsets.all(10),
+                // height: 25, // usuwam to ogranicza height kontenera
+                color: Colors.white,
+
+                child: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.phone,
+                        size: 20,
+                        color: Colors.cyan[900],
+                      ),
+                      SizedBox(
+                        width: 20,
+                      ),
+                      Text(
+                        '+44 876 231 234',
+                        style: TextStyle(
+                          color: Colors.cyan[900],
+                          fontFamily: 'SourceSansPro-SemiBold',
+                          fontWeight: FontWeight.bold,
+                          fontSize: 15,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              // Container(  // zamieniamy Container na Card
+          Card(
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 30),
+                // padding: EdgeInsets.all(10),
+                // height: 25, // usuwam to ogranicza height kontenera
+                color: Colors.white,
+
+                child: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.email,
+                        size: 20,
+                        color: Colors.cyan[900],
+                      ),
+                      SizedBox(
+                        width: 20,
+                      ),
+                      Text(
+                        'giga.chad@mail.ch',
+                        style: TextStyle(
+                          color: Colors.cyan[900],
+                          fontFamily: 'SourceSansPro-SemiBold',
+                          fontWeight: FontWeight.bold,
+                          fontSize: 15,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+            // mainAxisAlignment: MainAxisAlignment.center,
+          ),
+        ),
+      ),
+    );
+  }
+}
